2933aa7724bcfb3cd09d0a7bb71980279d277f13
rifqi <merembablas@gmail.com>
2024-05-02 21:04:28 +0700

    chore: update declarations canister

--- src/declarations/factory/factory.did.d.ts
+++ src/declarations/factory/factory.did.d.ts
@@ -2,15 +2,39 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
+export interface Account {
+  'owner' : Principal,
+  'subaccount' : [] | [Subaccount],
+}
+export interface AdvancedSettings {
+  'permitted_drift' : Timestamp,
+  'burned_tokens' : Balance,
+  'transaction_window' : Timestamp,
+}
+export type Balance = bigint;
+export interface Factory {
+  'back_loan' : ActorMethod<[PoolArgs], Principal>,
+  'get_owner' : ActorMethod<[], Principal>,
+  'get_pool_cycle' : ActorMethod<[], bigint>,
+  'get_pools' : ActorMethod<[bigint, bigint], Array<PoolRecord>>,
+  'propose_loan' : ActorMethod<[PoolArgs], undefined>,
+  'set_pool_cycle' : ActorMethod<[bigint], bigint>,
+  'transfer_ownership' : ActorMethod<[Principal], Principal>,
+  'unback_loan' : ActorMethod<[Principal], Principal>,
+}
+export interface PoolArgs {
+  'asset' : Principal,
+  'info' : PoolInfo,
+  'token_args' : TokenInitArgs,
+  'borrowers' : Array<Principal>,
+}
 export interface PoolInfo {
   'apr' : string,
   'title' : string,
   'issuer_picture' : string,
-  'smart_contract_url' : string,
   'total_loan_amount' : string,
   'payment_frequency' : string,
   'description' : string,
-  'borrower' : Principal,
   'maturity_date' : Time,
   'loan_term' : string,
   'issuer_description' : string,
@@ -25,11 +49,9 @@ export interface PoolRecord {
   'status' : PoolStatus,
   'title' : string,
   'issuer_picture' : string,
-  'smart_contract_url' : string,
   'total_loan_amount' : string,
   'payment_frequency' : string,
   'description' : string,
-  'borrower' : Principal,
   'maturity_date' : Time,
   'loan_term' : string,
   'issuer_description' : string,
@@ -38,16 +60,27 @@ export interface PoolRecord {
   'fundrise_end_time' : Time,
   'credit_rating' : string,
   'origination_date' : Time,
+  'borrowers' : Array<Principal>,
 }
 export type PoolStatus = { 'active' : null } |
   { 'upcoming' : null } |
   { 'pending' : null } |
   { 'inactive' : null } |
   { 'default' : null };
+export type Subaccount = Uint8Array | number[];
 export type Time = bigint;
-export interface _SERVICE {
-  'get_pools' : ActorMethod<[bigint, bigint], Array<PoolRecord>>,
-  'proposePool' : ActorMethod<[PoolInfo], Principal>,
+export type Timestamp = bigint;
+export interface TokenInitArgs {
+  'fee' : Balance,
+  'advanced_settings' : [] | [AdvancedSettings],
+  'decimals' : number,
+  'minting_account' : [] | [Account],
+  'name' : string,
+  'initial_balances' : Array<[Account, Balance]>,
+  'min_burn_amount' : Balance,
+  'max_supply' : Balance,
+  'symbol' : string,
 }
+export interface _SERVICE extends Factory {}
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- src/declarations/factory/factory.did.js
+++ src/declarations/factory/factory.did.js
@@ -1,4 +1,49 @@
 export const idlFactory = ({ IDL }) => {
+  const Time = IDL.Int;
+  const PoolInfo = IDL.Record({
+    'apr' : IDL.Text,
+    'title' : IDL.Text,
+    'issuer_picture' : IDL.Text,
+    'total_loan_amount' : IDL.Text,
+    'payment_frequency' : IDL.Text,
+    'description' : IDL.Text,
+    'maturity_date' : Time,
+    'loan_term' : IDL.Text,
+    'issuer_description' : IDL.Text,
+    'secured_by' : IDL.Text,
+    'fundrise_end_time' : Time,
+    'credit_rating' : IDL.Text,
+    'origination_date' : Time,
+  });
+  const Balance = IDL.Nat;
+  const Timestamp = IDL.Nat64;
+  const AdvancedSettings = IDL.Record({
+    'permitted_drift' : Timestamp,
+    'burned_tokens' : Balance,
+    'transaction_window' : Timestamp,
+  });
+  const Subaccount = IDL.Vec(IDL.Nat8);
+  const Account = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(Subaccount),
+  });
+  const TokenInitArgs = IDL.Record({
+    'fee' : Balance,
+    'advanced_settings' : IDL.Opt(AdvancedSettings),
+    'decimals' : IDL.Nat8,
+    'minting_account' : IDL.Opt(Account),
+    'name' : IDL.Text,
+    'initial_balances' : IDL.Vec(IDL.Tuple(Account, Balance)),
+    'min_burn_amount' : Balance,
+    'max_supply' : Balance,
+    'symbol' : IDL.Text,
+  });
+  const PoolArgs = IDL.Record({
+    'asset' : IDL.Principal,
+    'info' : PoolInfo,
+    'token_args' : TokenInitArgs,
+    'borrowers' : IDL.Vec(IDL.Principal),
+  });
   const PoolStatus = IDL.Variant({
     'active' : IDL.Null,
     'upcoming' : IDL.Null,
@@ -6,18 +51,15 @@ export const idlFactory = ({ IDL }) => {
     'inactive' : IDL.Null,
     'default' : IDL.Null,
   });
-  const Time = IDL.Int;
   const PoolRecord = IDL.Record({
     'id' : IDL.Principal,
     'apr' : IDL.Text,
     'status' : PoolStatus,
     'title' : IDL.Text,
     'issuer_picture' : IDL.Text,
-    'smart_contract_url' : IDL.Text,
     'total_loan_amount' : IDL.Text,
     'payment_frequency' : IDL.Text,
     'description' : IDL.Text,
-    'borrower' : IDL.Principal,
     'maturity_date' : Time,
     'loan_term' : IDL.Text,
     'issuer_description' : IDL.Text,
@@ -26,31 +68,22 @@ export const idlFactory = ({ IDL }) => {
     'fundrise_end_time' : Time,
     'credit_rating' : IDL.Text,
     'origination_date' : Time,
+    'borrowers' : IDL.Vec(IDL.Principal),
   });
-  const PoolInfo = IDL.Record({
-    'apr' : IDL.Text,
-    'title' : IDL.Text,
-    'issuer_picture' : IDL.Text,
-    'smart_contract_url' : IDL.Text,
-    'total_loan_amount' : IDL.Text,
-    'payment_frequency' : IDL.Text,
-    'description' : IDL.Text,
-    'borrower' : IDL.Principal,
-    'maturity_date' : Time,
-    'loan_term' : IDL.Text,
-    'issuer_description' : IDL.Text,
-    'secured_by' : IDL.Text,
-    'fundrise_end_time' : Time,
-    'credit_rating' : IDL.Text,
-    'origination_date' : Time,
-  });
-  return IDL.Service({
+  const Factory = IDL.Service({
+    'back_loan' : IDL.Func([PoolArgs], [IDL.Principal], []),
+    'get_owner' : IDL.Func([], [IDL.Principal], ['query']),
+    'get_pool_cycle' : IDL.Func([], [IDL.Nat], ['query']),
     'get_pools' : IDL.Func(
         [IDL.Nat, IDL.Nat],
         [IDL.Vec(PoolRecord)],
         ['query'],
       ),
-    'proposePool' : IDL.Func([PoolInfo], [IDL.Principal], []),
+    'propose_loan' : IDL.Func([PoolArgs], [], ['oneway']),
+    'set_pool_cycle' : IDL.Func([IDL.Nat], [IDL.Nat], []),
+    'transfer_ownership' : IDL.Func([IDL.Principal], [IDL.Principal], []),
+    'unback_loan' : IDL.Func([IDL.Principal], [IDL.Principal], []),
   });
+  return Factory;
 };
 export const init = ({ IDL }) => { return []; };
